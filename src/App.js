56219b6fb3eafb9e3cca93adb0ac5c47fd0c237d
github username <github email address>
2019-01-10 17:33:55 -0500

    install geolocation, add geolocation.js, update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Weather from './Weather';
+import Stocks from './Stocks'
+import Geolocation from './Geolocation'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+
+      <h3>
+        Emmanuel Owusu
+      </h3>
+
+        <div className = "work-profiles">
+          <a className="github-link" href="https://github.com/emanovic7" target="_blank" rel="noopener noreferrer">
+            Github
+          </a><br />
+          <a className="linkedin-link" href="https://www.linkedin.com/in/emmanuel-owusu-007/" target="_blank" rel="noopener noreferrer">
+            LinkedIn
           </a>
-        </header>
+        </div>
+
+
+          <Weather />
+          <Stocks />
+          <Geolocation />
+
+
+
+
       </div>
     );
   }

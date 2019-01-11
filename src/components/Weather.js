77083e87233ac139c3d3a1233997f0c9f72b9dd3
github username <github email address>
2019-01-11 18:38:45 -0500

    update this.state && this.setState, add temperature.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Geolocation from '../Geolocation'
 
 
 const API_KEY = "61c81d3631a11bb37b8536b428f3b45b"
@@ -11,7 +12,8 @@ class Weather extends React.Component{
       temperature: {
         degrees: null,
         wind: null,
-        city: null
+        city: null,
+        degs: null
     }
     };
   }
@@ -22,16 +24,19 @@ class Weather extends React.Component{
     {/*fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=61c81d3631a11bb37b8536b428f3b45b')*/}
     fetch('http://api.openweathermap.org/data/2.5/weather?lat=40.7428561&lon=-73.9270205&APPID=61c81d3631a11bb37b8536b428f3b45b')
     .then(response => response.json())
-    .then(({weather, wind, name}) => this.setState({
+    .then(({weather, wind, main, name}) => this.setState({
       temperature: {
         ...this.state.temperature,
         degrees: weather,
         wind: wind,
+        degs: main,
         city: name
       }
     })
     )
   }
+
+
 
 
   render(){
@@ -43,8 +48,12 @@ class Weather extends React.Component{
           this.state.temperature.degrees && this.state.temperature.degrees.map((item, id) =>
             <p key={id}>{item.main} -- {item.description}</p>)
           }
+          <p>Temperature: {this.state.temperature.degs && this.state.temperature.degs.temp} Kelvins</p>
           <p>City: {this.state.temperature && this.state.temperature.city}</p>
           <p>Wind Speed: {this.state.temperature.wind && this.state.temperature.wind.speed}</p>
+
+          
+
       </div>
     )
   }

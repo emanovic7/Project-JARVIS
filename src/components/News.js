662d2c7f735bc98a140b9d278676fc0accbd7699
github username <github email address>
2019-01-23 21:35:20 -0500

    remove buttons from News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
-import {Button} from 'semantic-ui-react'
+import {Button, Search} from 'semantic-ui-react'
 
 const API_KEY = '8aef1181042e4066a428c268e4e1078d'
 
 class News extends React.Component {
 
-  constructor(){
-    super()
+  constructor(props){
+    super(props);
     this.state = {
+      error: null,
+      isLoaded: false,
       news: null
     }
   }
@@ -16,20 +18,50 @@ class News extends React.Component {
     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=8aef1181042e4066a428c268e4e1078d')
     .then(response => response.json())
     .then(({articles}) => this.setState({
-      news: articles
-    }))
+        isLoaded: true,
+        news: articles
+      }),
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        })
+      }
+    );
+  };
+
+  handleTechnology = () => {
+    function componentDidMount(){
+      fetch('https://newsapi.org/v2/sources?apiKey=8aef1181042e4066a428c268e4e1078d')
+      .then(response => response.json())
+      .then(({sources}) => this.setState({
+        news: sources.category ==="technology"
+      }));
+    };
+    return(
+      <div>
+        {this.state.news}
+      </div>
+    )
   }
+
+
+
+
+
 
   render(){
 
     return(
-      <div className="news-articles">
-      <h3>Headlines</h3>
-      <ol>
-        add link to read "read"{this.state.news && this.state.news.slice(0,5).map((item) => <li><p>{item.title}</p></li>)}
-      </ol>
-      <Button content="World" /><Button content="Science"/><Button content="Politics"/><Button content="Entertainment"/>
-      </div>
+      <React.Fragment>
+        <div className="news-articles">
+            <h3>Headlines</h3>
+            <ol>
+              add link to read "read"{this.state.news && this.state.news.slice(0,5).map((item) => <li><p>{item.title}</p></li>)}
+            </ol>
+        </div>
+        <Search aligned="left" value="filter by subject"/>
+      </React.Fragment>
     )
   }
 }
